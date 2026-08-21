6f65e33c44ff9a1a76b500d80eb73291ab78c782
Nhan-DH <hongnhan474747@gmail.com>
2026-08-21 09:01:13 +0700

    fix : path

--- backend/src/modules/orders/order.entity.ts
+++ backend/src/modules/orders/order.entity.ts
@@ -7,7 +7,7 @@ import {
   ManyToOne,
   JoinColumn,
 } from 'typeorm';
-import { User } from '../../database/entities/user.entity';
+import { User } from '../../user/entities/user.entity';
 // Note: If you have an Artwork entity, you can add ManyToOne relation here later
 
 export enum OrderStatus {
